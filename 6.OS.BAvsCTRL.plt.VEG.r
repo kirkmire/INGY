3c03e1c83c5c4f5e193a421eafd4456189f72c2f
kirkmire <kirkmirecp@gmail.com>
2016-04-14 18:17:44 -0600

    updates

--- 6.OS.BAvsCTRL.plt.VEG.r
+++ 6.OS.BAvsCTRL.plt.VEG.r
@@ -17,34 +17,19 @@ OS.ctrl.plot<- merge(CTRLandGE.volume.index,sinst, by.x = "Installation", by.y =
 
 #Below is from 4.explor.plots (describes the OS in terms of BAPA post harvest)#
 library(plyr)
+
 BAPA.by.inst<-ddply(OverS, "Installation", summarise, BAPA.inst= sum(BAPA))
 
+BAPA.by.inst$ave.BAPA<-BAPA.by.inst$BAPA.inst/7
+
+
+bapa.by.inst<-BAPA.by.inst[!(BAPA.by.inst$Installation %in% drp),]
 
 #Merges OS data to form df with OS BAPA, SI, and US Volume#
 threeDVeg<-merge(BAPA.by.inst,OS.ctrl.plot, by.x = "Installation", by.y = "Installation",all=F)
 
-
 #Remove Installations#
 threeDVeg<-threeDVeg[!(threeDVeg$Installation %in% drp),]
-
-#Creates a 3-D Scatterplot (x=BAPA,y=SI, z=Volume#
-
-library(scatterplot3d)
-s3D<-scatterplot3d(threeDVeg$BAPA.inst,threeDVeg$SiteIndex_Value,threeDVeg$volume,
-type="h", lty.hplot=2,       # lines to the horizontal plane
-scale.y=.75,                 # scale y axis (reduce by 25%)
-main="3-D Scatterplot",
-xlab="Residual BAPA (ft^3/Ac)",
-ylab="Site Index Value",
-zlab="Volume Index",
-pch=as.integer(threeDVeg$Trt)
-)
-
-#Extracts coordinates from 3D plot for installation labels#
-s3D.coords <- s3D$xyz.convert(threeDVeg$BAPA.inst,threeDVeg$SiteIndex_Value,threeDVeg$volume)
-text(s3D.coords$x, s3D.coords$y,     # x and y coordinates
-     labels=(threeDVeg$Installation),       # text to plot
-     pos=4, cex=.5)    
 
 
 
@@ -55,7 +40,15 @@ ba.final<-merge(locdataX,ba.final,by="Installation")
 plotcolor<-brewer.pal(3,"RdYlBu")
 
 
-
+#Just SI vs BA#
+ggplot(threeDVeg, aes(x =ave.BAPA , y = SiteIndex_Value,color=threeDVeg$SiteIndex_Species))+
+  geom_point(size=2)+
+  ggtitle("Site Index vs Residual Basal Area")+
+  xlab("Residual Basal Area (ft^2/acre)")+
+  ylab("Site Index")+
+  labs(color="SI Species")+
+  theme(axis.text=element_text(size=12),
+        axis.title=element_text(size=14,face="bold"))
 
 
 
--- rgl.R
+++ rgl.R
@@ -8,7 +8,7 @@ myColorRamp <- function(colors, values) {
   rgb(x[,1], x[,2], x[,3], maxColorValue = 255)
 }
 
-plot3d(threeDVeg$SiteIndex_Value,threeDVeg$BAPA.inst,threeDVeg$volume, 
+plot3d(threeDVeg$SiteIndex_Value,threeDVeg$ave.BAPA,threeDVeg$volume, 
        axes=FALSE,size=7,
        col=myColorRamp(c("blue","green","yellow","red"),threeDVeg$volume),
        xlab="", ylab="", zlab="")
@@ -17,35 +17,36 @@ grid3d(side=c('x+', 'y-', 'z'), col="gray")
 title3d(xlab = "Site Index", ylab = "Residual BAPA",zlab = "Vegetation Volume (M^3)", col="red")
 
 #Adding vertical droplines#
-plot3d(threeDVeg$SiteIndex_Value,threeDVeg$BAPA.inst,threeDVeg$volume,type='h',add=T,
+plot3d(threeDVeg$SiteIndex_Value,threeDVeg$ave.BAPA,threeDVeg$volume,type='h',add=T,
        col=myColorRamp(c("blue","green","yellow","red"),threeDVeg$volume))
 
 #Fitting&Plotting Treatment Planes#
 
 just.ctrl<-threeDVeg[!(threeDVeg$Trt=='GE'),]
 
-fit1=lm(volume~BAPA.inst+SiteIndex_Value,data=just.ctrl)
+fit1=lm(volume~ave.BAPA+SiteIndex_Value,data=just.ctrl)
 summary(fit1)
 
 coefs <- coef(fit1)
-planes3d(a=coefs["SiteIndex_Value"], b=coefs["BAPA.inst"],-1, coefs["(Intercept)"], alpha=0.50, col="plum2")
+planes3d(a=coefs["SiteIndex_Value"], b=coefs["ave.BAPA"],-1, coefs["(Intercept)"], alpha=0.50, col="plum2")
 
 just.ge<-threeDVeg[!(threeDVeg$Trt=='CTRL'),]
 
-fit2=lm(volume~BAPA.inst+SiteIndex_Value,data=just.ge)
+fit2=lm(volume~ave.BAPA+SiteIndex_Value,data=just.ge)
 summary(fit1)
 
 coefs <- coef(fit2)
-planes3d(a=coefs["SiteIndex_Value"], b=coefs["BAPA.inst"],-1, coefs["(Intercept)"], alpha=0.50, col="red")
+planes3d(a=coefs["SiteIndex_Value"], b=coefs["ave.BAPA"],-1, coefs["(Intercept)"], alpha=0.50, col="red")
 
 
 #making a movie#
 
 # Create a movie
 
-movie3d(spin3d(axis = c(0, 0, 1)), duration = 3,
+movie3d(spin3d(axis = c(0, 0, 1)), duration = 15,
         dir = getwd())
 
+getwd()
 
 #Extracts coordinates from 3D plot for installation labels#
 #s3D.coords <- s3D$xyz.convert(threeDVeg$BAPA.inst,threeDVeg$SiteIndex_Value,threeDVeg$volume)
@@ -54,15 +55,7 @@ movie3d(spin3d(axis = c(0, 0, 1)), duration = 3,
 #    pos=4, cex=.5)    
 
 dev.off()
-#Just SI vs BA#
-ggplot(threeDVeg, aes(x =BAPA.inst , y = SiteIndex_Value,color=threeDVeg$SiteIndex_Species))+
-  geom_point(size=2)+
-  ggtitle("Site Index vs Residual Basal Area")+
-  xlab("Residual Basal Area (ft^2/acre)")+
-  ylab("Site Index")+
-  labs(color="SI Species")+
-  theme(axis.text=element_text(size=12),
-        axis.title=element_text(size=14,face="bold"))
+
 
 
 

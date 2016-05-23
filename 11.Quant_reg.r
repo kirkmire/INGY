75d8c6be034e92c6a186af7b39d41d43a47e04ee
kirkmire <kirkmirecp@gmail.com>
2016-05-23 14:29:06 -0600

    latest

--- 11.Quant_reg.r
+++ 11.Quant_reg.r
@@ -4,35 +4,36 @@
 
 merged_stagm_stag <- merge(stagm, stag,by=c("Installation","Plot","STP","Tree"))
 
-LL<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(7,3)&
-                        merged_stagm_stag$Year_Measurement%in%c(2001,2008),]
+LL<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(1:7)&
+                        merged_stagm_stag$Year_Measurement%in%c(2004,2008),]
 
-
-                      
+(min(LL$Year_Measurement))
                    
+
+library(plyr)
+
+test_df<-ddply(LL, .(LL$Installation, LL$Plot, LL$STP, LL$Tree), summarize, -diff(Height_Total))
 
 
 
 dev.off()
 
 
-
-
-LL_initial<-LL$Height_Total[LL$Year_Measurement==2001]
-LL_growth<-LL$Height_Total[LL$Year_Measurement==2008]
+#LL_initial<-LL$Height_Total[LL$Year_Measurement==2004]
+#LL_growth<-LL$Height_Total[LL$Year_Measurement==2008]
 
 
 library(quantreg)
 
 help(rq)
 
-plot(LL_initial,LL_growth,cex=.25,type="n",xlab="Initial Height",ylab="Hieght Growth")
-points(LL_initial,LL_growth,cex=.5,col="blue")
-abline(rq(LL_growth~LL_initial, tau=.5),col="blue")
-abline(lm(LL_growth~LL_initial),lty=2,col="red")
+plot(LL_initial,test_df$..1,cex=.25,type="n",xlab="Initial Height",ylab="Height Growth")
+points(LL_initial,test_df$..1,cex=.5,col="blue")
+abline(rq(test_df$..1~LL_initial, tau=.5),col="blue")
+abline(lm(test_df$..1~LL_initial),lty=2,col="red")
 taus<-c(.05,.1,.25,.75,.9,.95)
 for(i in 1:length(taus)){
-  abline(rq(LL_growth~LL_initial,tau=taus[i]),col="gray")
+  abline(rq(test_df$..1~LL_initial,tau=taus[i]),col="gray")
 }
-fit2<-summary(rq(LL_growth~LL_initial,tau=c(.05,.25,.5,.75,.95)))
+fit2<-summary(rq(test_df$..1~LL_initial,tau=c(.05,.25,.5,.75,.95)))
 

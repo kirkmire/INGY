01cf0ea7df1c2f352141835a8051445d64d17f13
kirkmire <kirkmirecp@gmail.com>
2016-05-24 11:47:48 -0600

    latest

--- 11.Quant_reg.r
+++ 11.Quant_reg.r
@@ -7,31 +7,37 @@ merged_stagm_stag <- merge(stagm, stag,by=c("Installation","Plot","STP","Tree"))
 LL<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(1:7)&
                         merged_stagm_stag$Year_Measurement%in%c(2004,2008),]
 
+LL_min<-LL[which(LL$Year_Measurement==min(LL$Year_Measurement)),]
 
-                   
+LL_max<-LL[which(LL$Year_Measurement==max(LL$Year_Measurement)),]
 
-library(plyr)
+LL_both<-merge(LL_min,LL_max, by=c("Installation","Plot","STP","Tree"))
 
-test_df<-ddply(LL, .(Installation, Plot, STP, Tree), summarize, abs(diff(Height_Total)))
+#Merge with splot for treatment column#
+LL_both<-merge(LL_both,splot,by=c("Installation","Plot"))
 
-LL2<-cbind(test_df,LL_initial)
+#Create Column with growth increment#
+LL_both$inc<-LL_both$Height_Total.y-LL_both$Height_Total.x
 
-LL2<-merge(LL2,splot, by=c("Installation","Plot"))
+boxplot(LL_both$inc)
 
-dev.off()
+#Appears to be errors in data entry
+#Tree 663 goes from 8.5ft to 1.6ft, should read 9.6ft
+
+LL$Height_Total[LL$Year_Measurement==2008&LL$Tree==663]<-9.6
 
 
-#LL_initial<-LL$Height_Total[LL$Year_Measurement==2004]
-#LL_growth<-LL$Height_Total[LL$Year_Measurement==2008]
 
 
+
+#Quantreg with default plotting#
 library(quantreg)
 
 help(rq)
 
 LL2$Treatment<-as.factor(LL2$Treatment)
 
-plot(LL2$LL_initial,LL2$..1,cex=.5,pch=19,xlab="Initial Height", ylab="Height Growth",col=LL2$Treatment)
+plot(LL_both$,LL2$..1,cex=.5,pch=19,xlab="Initial Height", ylab="Height Growth",col=LL2$Treatment)
 points(LL2$LL_initial,LL2$..1,cex=.5)
 abline(rq(LL2$..1~LL2$LL_initial, tau=.5), col="blue")
 abline(lm(LL2$..1~LL2$LL_initial),lty=2, col="red")

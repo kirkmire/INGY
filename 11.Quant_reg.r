4ca4094d7d3e1e2294bf4bfc2c2f9590e16f04e5
kirkmire <kirkmirecp@gmail.com>
2016-05-23 18:01:08 -0600

    latest

--- 11.Quant_reg.r
+++ 11.Quant_reg.r
@@ -7,14 +7,16 @@ merged_stagm_stag <- merge(stagm, stag,by=c("Installation","Plot","STP","Tree"))
 LL<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(1:7)&
                         merged_stagm_stag$Year_Measurement%in%c(2004,2008),]
 
-(min(LL$Year_Measurement))
+
                    
 
 library(plyr)
 
-test_df<-ddply(LL, .(LL$Installation, LL$Plot, LL$STP, LL$Tree), summarize, -diff(Height_Total))
+test_df<-ddply(LL, .(Installation, Plot, STP, Tree), summarize, abs(diff(Height_Total)))
 
+LL2<-cbind(test_df,LL_initial)
 
+LL2<-merge(LL2,splot, by=c("Installation","Plot"))
 
 dev.off()
 
@@ -27,13 +29,37 @@ library(quantreg)
 
 help(rq)
 
-plot(LL_initial,test_df$..1,cex=.25,type="n",xlab="Initial Height",ylab="Height Growth")
-points(LL_initial,test_df$..1,cex=.5,col="blue")
-abline(rq(test_df$..1~LL_initial, tau=.5),col="blue")
-abline(lm(test_df$..1~LL_initial),lty=2,col="red")
+LL2$Treatment<-as.factor(LL2$Treatment)
+
+plot(LL2$LL_initial,LL2$..1,cex=.5,pch=19,xlab="Initial Height", ylab="Height Growth",col=LL2$Treatment)
+points(LL2$LL_initial,LL2$..1,cex=.5)
+abline(rq(LL2$..1~LL2$LL_initial, tau=.5), col="blue")
+abline(lm(LL2$..1~LL2$LL_initial),lty=2, col="red")
 taus<-c(.05,.1,.25,.75,.9,.95)
 for(i in 1:length(taus)){
-  abline(rq(test_df$..1~LL_initial,tau=taus[i]),col="gray")
+  abline(rq(LL2$..1~LL2$LL_initial,tau=taus[i],data=LL_trees),col="gray")
 }
-fit2<-summary(rq(test_df$..1~LL_initial,tau=c(.05,.25,.5,.75,.95)))
+fit2<-summary(rq(LL2$..1~LL2$LL_initial,tau=c(.05,.25,.5,.75,.95)))
+
+
+#Quantile Regression Plot on GGplot#
+library(ggplot2)
+
+
+ggplot(LL2, aes(x=LL2$LL_initial,y=LL2$..1))+
+  geom_point(size=2,aes(col=LL2$Treatment))+
+  ggtitle("Growth Inc vs Initial Height")+
+  xlab("Initial Height")+
+  ylab("Growth Inc")+
+  geom_smooth(method=lm,aes(x=LL2$LL_initial,y=LL2$..1,col=LL2$Treatment))+
+  stat_quantile(quantiles = taus)
+
+#Warning generated from removal of Height-HeightNA#
+#Okay#
+
+stat_quantile(aes(colour = ..quantile..), quantiles = taus)
+
+
+
+
 

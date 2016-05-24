f8a55facba9b883863b2cd8b847070d1421f330f
kirkmire <kirkmirecp@gmail.com>
2016-05-24 12:41:53 -0600

    LL_done

--- 11.Quant_reg.r
+++ 11.Quant_reg.r
@@ -6,6 +6,23 @@ merged_stagm_stag <- merge(stagm, stag,by=c("Installation","Plot","STP","Tree"))
 
 LL<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(1:7)&
                         merged_stagm_stag$Year_Measurement%in%c(2004,2008),]
+#Appears to be errors in data entry
+#Tree 663 goes from 8.5ft to 1.6ft, should read 9.6ft
+
+LL$Height_Total[LL$Year_Measurement==2008&LL$Plot==1&LL$Tree==663]<-9.6
+
+#Tree 53 goes from 8.6ft to 1.5ft, should read 9.5ft
+
+LL$Height_Total[LL$Plot==3&LL$Tree==53&LL$Year_Measurement==2008]<-9.5
+
+#Tree 19, Plot 3 has Height total of 0 for 2008 measurement, removed#
+
+LL<-LL[!(LL$Plot=="3"&LL$Tree=="19"),]
+
+#Tree 242, Plot 6 has Height total of 0 for 2008 measurement, removed#
+
+LL<-LL[!(LL$Plot=="6"&LL$Tree=="242"),]
+
 
 LL_min<-LL[which(LL$Year_Measurement==min(LL$Year_Measurement)),]
 
@@ -21,13 +38,6 @@ LL_both$inc<-LL_both$Height_Total.y-LL_both$Height_Total.x
 
 boxplot(LL_both$inc)
 
-#Appears to be errors in data entry
-#Tree 663 goes from 8.5ft to 1.6ft, should read 9.6ft
-
-LL$Height_Total[LL$Year_Measurement==2008&LL$Tree==663]<-9.6
-
-
-
 
 
 #Quantreg with default plotting#
@@ -35,29 +45,29 @@ library(quantreg)
 
 help(rq)
 
-LL2$Treatment<-as.factor(LL2$Treatment)
 
-plot(LL_both$,LL2$..1,cex=.5,pch=19,xlab="Initial Height", ylab="Height Growth",col=LL2$Treatment)
-points(LL2$LL_initial,LL2$..1,cex=.5)
-abline(rq(LL2$..1~LL2$LL_initial, tau=.5), col="blue")
-abline(lm(LL2$..1~LL2$LL_initial),lty=2, col="red")
+plot(LL_both$Height_Total.x,LL_both$inc ,cex=.5,pch=19,xlab="Initial Height", ylab="Height Growth Increment",
+     col=LL_both$Treatment)
+points(LL_both$Height_Total.x,LL_both$inc,cex=.8)
+abline(rq(LL_both$inc~LL_both$Height_Total.x, tau=.5), col="blue")
+abline(lm(LL_both$inc~LL_both$Height_Total.x),lty=2, col="red")
 taus<-c(.05,.1,.25,.75,.9,.95)
 for(i in 1:length(taus)){
-  abline(rq(LL2$..1~LL2$LL_initial,tau=taus[i],data=LL_trees),col="gray")
+  abline(rq(LL_both$inc~LL_both$Height_Total.x,tau=taus[i],data=LL_both),col="gray")
 }
-fit2<-summary(rq(LL2$..1~LL2$LL_initial,tau=c(.05,.25,.5,.75,.95)))
+fit2<-summary(rq(LL_both$inc~LL_both$Height_Total.x,tau=c(.05,.25,.5,.75,.95)))
 
 
 #Quantile Regression Plot on GGplot#
 library(ggplot2)
 
 
-ggplot(LL2, aes(x=LL2$LL_initial,y=LL2$..1))+
-  geom_point(size=2,aes(col=LL2$Treatment))+
+ggplot(LL_both, aes(x=LL_both$Height_Total.x,y=LL_both$inc))+
+  geom_point(size=2,aes(col=LL_both$Treatment))+
   ggtitle("Growth Inc vs Initial Height")+
   xlab("Initial Height")+
   ylab("Growth Inc")+
-  geom_smooth(method=lm,aes(x=LL2$LL_initial,y=LL2$..1,col=LL2$Treatment))+
+  geom_smooth(method=lm,aes(x=LL_both$Height_Total.x,y=LL_both$inc,col=LL_both$Treatment))+
   stat_quantile(quantiles = taus)
 
 #Warning generated from removal of Height-HeightNA#

0e1ad2077e6444710004e646371963920f83982f
kirkmire <kirkmirecp@gmail.com>
2016-05-24 17:00:07 -0600

    small changes

--- 11.Quant_reg_LL.r
+++ 11.Quant_reg_LL.r
@@ -105,10 +105,10 @@ dev.off()
 #now plot the two quantile functions to compare
 par(mfrow = c(1,2))
 plot(c(ps,ps),c(qs.poor,qs.rich), type="n",
-+ xlab = expression(tau), ylab = "quantile")
+xlab = expression(tau), ylab = "quantile")
 plot(stepfun(ps,c(qs.poor[1],qs.poor)), do.points=FALSE, add=TRUE)
 plot(stepfun(ps,c(qs.poor[1],qs.rich)), do.points=FALSE, add=TRUE,
-       + col.hor = "gray", col.vert = "gray")
+col.hor = "gray", col.vert = "gray")
 
 ## now plot associated conditional density estimates
 ## weights from ps (process)
@@ -116,7 +116,7 @@ ps.wts <- (c(0,diff(ps)) + c(diff(ps),0)) / 2
 ap <- akj(qs.poor, z=qs.poor, p = ps.wts)
 ar <- akj(qs.rich, z=qs.rich, p = ps.wts)
 plot(c(qs.poor,qs.rich),c(ap$dens,ar$dens),type="n",
-       + xlab= "Food Expenditure", ylab= "Density")
+    xlab= "Incremental Growth", ylab= "Density")
 lines(qs.rich, ar$dens, col="gray")
 lines(qs.poor, ap$dens, col="black")
 legend("topright", c("poor","rich"), lty = c(1,1), col=c("black","gray"))

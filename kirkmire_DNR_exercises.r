e0f119afff16b30ab8c6343c4d17638bb9135e8d
kirkmire <kirkmirecp@gmail.com>
2016-05-28 14:13:36 -0600

    latest

--- kirkmire_DNR_exercises.r
+++ kirkmire_DNR_exercises.r
@@ -5,13 +5,13 @@ tree_data<-read.csv(url("http://forestlidar.org/added_files/tree_data.csv"))
 
 ##Exercise 2
 
-myfunction <- function(x){
+function1 <- function(x){
   dbh<-c(min(x[,2]),max(x[,2]),mean(x[,2]))
   ht<-c(min(x[,3]),max(x[,3]),mean(x[,3]))
   age<-c(min(x[,4]),max(x[,4]),mean(x[,4]))
   x_coord<-c(min(x[,5]),max(x[,5]),mean(x[,5]))
   y_coord<-c(min(x[,6]),max(x[,6]),mean(x[,6]))
-  grt_than<-length(which(X[,2]>25))
+  grt_than<-length(which(x[,2]>25))
   numb_grt_25<-c(grt_than,0,0)
   vec<-c(dbh,ht,age,x_coord,y_coord,numb_grt_25)
   matrix1<-matrix(vec,nrow=3,ncol=6,dimnames=list(c("Min","Max","Avg"),
@@ -20,7 +20,7 @@ myfunction <- function(x){
     return(matrix2)
 }
 
-myfunction(tree_data)
+function1(tree_data)
 
 
 ##Exercise 3
@@ -31,8 +31,7 @@ avg_ht_function <- function(lower_bound,upper_bound){
   return(avg_ht)
 }
   
-avg_ht_function(1,5)  
-  
+
 avg_ht_function(5,10)    
 
 avg_ht_function(20,30)         
@@ -45,24 +44,17 @@ avg_ht_function_clean <- function(lower_bound,upper_bound){
   return(avg_ht)
 }
 
-avg_ht_function_clean("a",10)  
+avg_ht_function("a",10)  
 
 avg_ht_function_clean(9999,99999)  
 
 ##Exercise 5
 
 # load the required libraries 
-library(sp) 
-library(rgdal)
-
-
+#library(sp) 
+#library(rgdal)
 library(maptools)
 
-
-http://forestlidar.org/added_files/wa_state.zip
-
-
-library(maptools)
 tmpdir <- tempdir()
 url <- 'http://forestlidar.org/added_files/wa_state.zip'
 file <- basename(url)
@@ -73,57 +65,6 @@ unzip(file, exdir = tmpdir )
 shapeFile <- paste(tmpdir,"/wa_state",sep="")
 area<- readShapeSpatial(shapeFile)
 
-library(ggplot2)
-library(ggmap)
-area.points <- fortify(area)
-
-library(RColorBrewer)
-colors <- brewer.pal(9, "BuGn")
-
-
-mapImage <- get_map(location = c(lon = -118, lat = 37.5),
-                    color = "color",
-                    source = "osm",
-                    # maptype = "terrain",
-                    zoom = 6)
-
-ggmap(mapImage) +
-  geom_polygon(aes(x = long,
-                   y = lat,
-                   group = group),
-               data = area.points,
-               color = colors[9],
-               fill = colors[6],
-               alpha = 0.5) +
-  labs(x = "Longitude",
-       y = "Latitude")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+plot(area)
+points(tree_data$x, tree_data$y, col = "red", 
+       cex = .5, pch=4) 

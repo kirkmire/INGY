488d35e9c1ee980f7c487dfc83230f0142542a2b
kirkmire <kirkmirecp@gmail.com>
2016-06-07 13:55:23 -0600

    ugh

--- 16.height.inc.vs.meas.yr.lines2016jun7.r
+++ 16.height.inc.vs.meas.yr.lines2016jun7.r
@@ -15,6 +15,7 @@ LL1_both$inc<-LL1_both$Height_Total.y-LL1_both$Height_Total.x
 #Merge with splot for treatment column#
 LL1_both<-merge(LL1_both,splot,by=c("Installation","Plot"))
 
+LL1_both<-LL1_both[!LL1_both$BasalDiameter=="NA",]
 boxplot(LL1_both$inc)
 
 ###Plotting
@@ -29,6 +30,33 @@ lllp<-ggplot(data=LL1_both, aes(x=Year_Measurement, y=inc,color=Treatment))+geom
   labs(title="LL Tree Height Inc over Meas Years",y="Height (ft)", x = "Measurement Year")+ 
   scale_fill_brewer(palette="Dark2") +tbp+ geom_line(aes(group=LL1_both$Tree))+ylim(0,10)
 lllp
+
+##trying some 3D stuff
+
+library(rgl)
+
+# color ramp
+myColorRamp <- function(colors, values) {
+  v <- (values - min(values))/diff(range(values))
+  x <- colorRamp(colors)(v)
+  rgb(x[,1], x[,2], x[,3], maxColorValue = 255)
+}
+
+plot3d(threeDVeg$SiteIndex_Value,threeDVeg$ave.BAPA,threeDVeg$volume, 
+       axes=FALSE,size=7,
+       col=myColorRamp(c("blue","green","yellow","red"),threeDVeg$volume),
+       xlab="", ylab="", zlab="")
+axes3d(c("x+", "y-", "z-"))
+grid3d(side=c('x+', 'y-', 'z'), col="gray")
+title3d(
+  #ylab = "Residual BAPA (ft/acre)",
+  # zlab = "Vegetation Volume (ft^3/ft^2)",
+  #xlab = "Site Index (ft)",
+  col="red")
+
+#Adding vertical droplines#
+plot3d(threeDVeg$SiteIndex_Value,threeDVeg$ave.BAPA,threeDVeg$volume,type='h',add=T,
+       col=myColorRamp(c("blue","green","yellow","red"),threeDVeg$volume))
 
 
 

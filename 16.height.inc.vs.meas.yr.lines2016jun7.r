4d9d1511e4e25c88d5168619d1e9d467bcbebe19
kirkmire <kirkmirecp@gmail.com>
2016-06-08 21:45:53 -0600

    another $

--- 16.height.inc.vs.meas.yr.lines2016jun7.r
+++ 16.height.inc.vs.meas.yr.lines2016jun7.r
@@ -1,4 +1,11 @@
 ###
+source('C:/Users/Colin/Desktop/R-Projects/INGY/1.readdatabase.2016jun2.r', echo=F)
+
+merged_stagm_stag <- merge(stagm, stag,by=c("Installation","Plot","STP","Tree"))
+
+
+LL<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(1:7)&merged_stagm_stag$Year_Measurement%in%c(1999,2001,2004,2008,2012),]
+
 
 #removing certain trees
 #Appears to be errors in data entry
@@ -14,20 +21,30 @@ LL<-LL[!(LL$Plot=="6"&LL$Tree=="242"),]
 LL<-LL[!(LL$Plot=="4"&LL$Tree=="683"),]
 #tree 111 plot 1 stp 6#
 LL<-LL[!(LL$Plot=="1"&LL$Tree=="111"),]
-LL1<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(1:7)&merged_stagm_stag$Year_Measurement%in%c(1999,2001,2004,2008,2012),]
-LL_init<-length(LL1[LL1$Year_Measurement=="1999",])
-LL1_both<-merge(LL1_init,LL1, by=c("Plot","STP","Tree"))
+
+
+
+LL1_init<-(LL[LL$Year_Measurement=="1999",])
+
+
+
+LL1_both<-merge(LL1_init,LL, by=c("Plot","STP","Tree"))
+
 LL1_both$inc<-LL1_both$Height_Total.y-LL1_both$Height_Total.x
+
+
+
 
 #Merge with splot for treatment column#
 LL1_both<-merge(LL1_both,splot,by=c("Installation","Plot"))
-LL1_both<-LL1_both[!LL1_both$BasalDiameter=="NA",]
+LL1_both<- LL1_both[!(is.na(LL1_both$BasalDiameter.x)) ,]
+
 boxplot(LL1_both$inc)
 
 
-
-
 ###Plotting
+library(ggplot2)
+
 
 tbp<-theme(                              
   axis.title.x = element_text(face="bold", color="black", size=16),
@@ -35,10 +52,10 @@ tbp<-theme(
   plot.title = element_text(face="bold", color = "black", size=18), 
   legend.position="bottom",axis.title.y=element_text(vjust=0.8))
 
-lllp<-ggplot(data=LL1_both, aes(x=Year_Measurement, y=inc,color=LL1_both$Height_Total.x))+geom_point()+
+lllp<-ggplot(data=LL1_both, aes(x=Year_Measurement.y, y=inc,color=LL1_both$Height_Total.x))+geom_point()+
   labs(title="LL Tree Height Inc over Meas Years",y="Height (ft)", x = "Measurement Year")+ 
   scale_fill_brewer(palette="Dark2") +tbp+ geom_line(aes(group=LL1_both$Tree))+ylim(0,10)
-lllp+scale_colour_gradient(limits=c(-1, 15), low="red", high="green")
+lllp+scale_colour_gradient(limits=c(0, 12), low="red", high="green")
 
 
 #why are so many trees missing in the upper years?

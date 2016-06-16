a481d3e29f2cd1b1c299c854079a5166c0a4e427
kirkmire <kirkmirecp@gmail.com>
2016-06-16 15:13:57 -0600

    presentation work 6/16

--- 11.Quant_reg_LL.2016jun10.r
+++ 11.Quant_reg_LL.2016jun10.r
@@ -10,26 +10,6 @@ merged_stagm_stag <- merge(stagm, stag,by=c("Installation","Plot","STP","Tree"))
 LL<-merged_stagm_stag[merged_stagm_stag$Installation=="LL"&merged_stagm_stag$Species=="PIPO"&merged_stagm_stag$Plot%in%c(1:7)&merged_stagm_stag$Year_Measurement%in%c(2004,2008),]
 
 
-#Appears to be errors in data entry
-#Tree 663 goes from 8.5ft to 1.6ft, should read 9.6ft
-LL$Height_Total[LL$Year_Measurement==2008&LL$Plot==1&LL$Tree==663]<-9.6
-
-#Tree 53 goes from 8.6ft to 1.5ft, should read 9.5ft
-LL$Height_Total[LL$Plot==3&LL$Tree==53&LL$Year_Measurement==2008]<-9.5
-
-#Tree 19, Plot 3 has Height total of 0 for 2008 measurement, removed#
-LL<-LL[!(LL$Plot=="3"&LL$Tree=="19"),]
-
-#Tree 242, Plot 6 has Height total of 0 for 2008 measurement, removed#
-LL<-LL[!(LL$Plot=="6"&LL$Tree=="242"),]
-
-#tree 683 plot 4 stp 5#
-LL<-LL[!(LL$Plot=="4"&LL$Tree=="683"),]
-
-#tree 111 plot 1 stp 6#
-LL<-LL[!(LL$Plot=="1"&LL$Tree=="111"),]
-
-
 LL_min<-LL[which(LL$Year_Measurement==min(LL$Year_Measurement)),]
 LL_max<-LL[which(LL$Year_Measurement==max(LL$Year_Measurement)),]
 LL_both<-merge(LL_min,LL_max, by=c("Installation","Plot","STP","Tree"))
---
+++ 18.database.error.corrections.2016jun16.r
@@ -0,0 +1,18 @@
+#Appears to be errors in data entry
+#Tree 663 goes from 8.5ft to 1.6ft, should read 9.6ft
+stagm$Height_Total[stagm$Year_Measurement==2008&stagm$Plot==1&stagm$Tree==663]<-9.6
+
+#Tree 53 goes from 8.6ft to 1.5ft, should read 9.5ft
+stagm$Height_Total[stagm$Plot==3&stagm$Tree==53&stagm$Year_Measurement==2008]<-9.5
+
+#Tree 19, Plot 3 has Height total of 0 for 2008 measurement, removed#
+stagm<-stagm[!(stagm$Plot=="3"&stagm$Tree=="19"),]
+
+#Tree 242, Plot 6 has Height total of 0 for 2008 measurement, removed#
+stagm<-stagm[!(stagm$Plot=="6"&stagm$Tree=="242"),]
+
+#tree 683 plot 4 stp 5#
+stagm<-stagm[!(stagm$Plot=="4"&stagm$Tree=="683"),]
+
+#tree 111 plot 1 stp 6#
+stagm<-stagm[!(stagm$Plot=="1"&stagm$Tree=="111"),]

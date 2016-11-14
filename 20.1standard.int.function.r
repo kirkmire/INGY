549948614d75cdb6eb375effdb79bbd2567f63ae
kirkmire <kirkmirecp@gmail.com>
2016-11-14 10:50:16 -0700

    11/14

--- 20.1standard.int.function.r
+++ 20.1standard.int.function.r
@@ -1,35 +1,7 @@
-#This function checks the year of measurement in the tree record against
-#the measurement timeline df to assign an interval length that can later 
-#be used to standardize the growth intervals to one year 
-
-int.function<-function(inst,year){
-  inst.df<-timeline[timeline$Installation==inst,]
-  inst.df<-as.data.frame(inst.df)
-  inst.df <- inst.df[!is.na(inst.df$Year_Measurement),]
-  if (year==min(inst.df$Year_Measurement)) {
-    interval<-0
-  } else {
-    next.small<-max(inst.df$Year_Measurement[which(inst.df$Year_Measurement<year)])
-    interval<-year-next.small
-  }
-    return(interval)
-}
 
 merged_stagm_stag$Installation<-as.character(merged_stagm_stag$Installation)
 timeline$Installation<-as.character(timeline$Installation)
 
-
-int.function(merged_stagm_stag$Installation[3],merged_stagm_stag$Year_Measurement[3])
-
-#example
-int.function(inst="DF",2006)
-
-#apply function across all rows
-
-interval<-mapply(int.function, merged_stagm_stag$Installation, merged_stagm_stag$Year_Measurement)     
-      
-#assign interval values to tree record column
-merged_stagm_stag$interval<-interval 
 
 #concate Inst,Plot,STP,Tree
 merged_stagm_stag$conc<-paste(merged_stagm_stag$Installation,merged_stagm_stag$Plot,merged_stagm_stag$STP,
@@ -39,6 +11,11 @@ merged_stagm_stag$conc<-paste(merged_stagm_stag$Installation,merged_stagm_stag$P
 
 merged_stagm_stag<-merged_stagm_stag[!merged_stagm_stag$Year_Measurement==merged_stagm_stag$Year_Growth,]
 merged_stagm_stag<-merged_stagm_stag[! merged_stagm_stag$Installation %in% drp,]
+
+
+#This function checks the year of measurement in the tree record against
+#the measurement timeline and finds the previous measurement year's height,
+#then subtracts it from the year of measurement to provide a height increment
 
 annual.ht<-function(conca,year){
   treeinfo<-merged_stagm_stag[merged_stagm_stag$conc==conca,]
@@ -58,14 +35,12 @@ annual.ht<-function(conca,year){
 }
   
 
-
 ###Example on a single tree record
 annual.ht("LR,2,1,123",2010)
 
 #Remove records of DF plot 4 stp 1 w/ year growth=2001 due to duplicate records of year meas=2002, bees nest
 merged_stagm_stag<-merged_stagm_stag[!(merged_stagm_stag$Installation=='DF'&merged_stagm_stag$Plot==4&
                     merged_stagm_stag$STP==1&merged_stagm_stag$Year_Growth==2001),]
-
 
 
 #Assign column for height annualized height growth inc
@@ -80,8 +55,7 @@ for(i in 1:nrow(merged_stagm_stag)){
 merged_stagm_stag$ht_annual[is.na(merged_stagm_stag$ht_annual)]<--999     
 merged_stagm_stag<-merged_stagm_stag[!(merged_stagm_stag$ht_annual==-999),]
 
-#Rename DF something reasonable
-
+#Rename dataframe something reasonable
 annual.gr<-merged_stagm_stag
               
                                      

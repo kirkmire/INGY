4af02fad1510e693c706bff8a23e8578fbd5d245
kirkmire <kirkmirecp@gmail.com>
2016-11-29 15:00:47 -0700

    11/29

--- 26.UV.variable.selection.r
+++ 26.UV.variable.selection.r
@@ -1,16 +1,31 @@
+
+######Understory Non-Tree Variables######
+#Percentage cover 1m, upper and lower extent of height
+
+
+
+
 #Selecting installations 
 veg<-c("EM","BC","TJ","RM","CM","TC")
 
 annual.gr3<-annual.gr[annual.gr$Installation %in% sim, ]
 
-#Merge Control&GE plots with 1m^2 plot data#
-plots_veg<- merge(annual.gr3,sstp1,by=c("Installation","Plot","STP","Year_Measurement"))
 
-#Aggregate the control and GE plots 4m^2 data by Installation and find the mean of top height and polyveg% cover#
-veg.variables<-aggregate(annual.gr3[, 17:19], 
-                                  list(LL_plots_veg$Installation,
-                                       LL_plots_veg$Treatment,
-                                       LL_plots_veg$Year_Measurement
-                                  ), mean, na.rm=TRUE)
+#Reshape 1 meter small tree plot veg measurements so that each stp record year has its
+#own associated Forb, Grass, Low Shrub, High Shrub and Polyveg measurements
 
-ht.class<-aggregate(Count~Installation+Plot+STP+Year_Measurement+HeightClass,data=sstpt,sum)
+stp<-reshape(sstp1, direction="wide",idvar=
+                     c("Installation","Plot","STP","Year_Measurement"),
+                   timevar="Lifeform",v.names=c("Coverage","Base","Top"))
+
+#Merge plot history and stp 1m veg
+
+veg_record<- merge(splot, stp,by=c("Installation","Plot"))
+
+
+#Merge annual small tree growth with veg_record for each year
+
+veg_record1<- merge(annual.gr3, veg_record,by=c("Installation","Plot","STP","Year_Measurement"))
+
+
+

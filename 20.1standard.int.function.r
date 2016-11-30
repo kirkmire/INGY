e7c4be7a9b24d1675f3073c38a584bb533dea890
kirkmire <kirkmirecp@gmail.com>
2016-11-29 19:47:43 -0700

    revised code notation and structure for clarity

--- 20.1standard.int.function.r
+++ 20.1standard.int.function.r
@@ -1,31 +1,44 @@
 
+
+#Makes "Installation" a character vector for both timeline and tree_records
 merged_stagm_stag$Installation<-as.character(merged_stagm_stag$Installation)
 timeline$Installation<-as.character(timeline$Installation)
 
+#Removes tree records that are from dropped installations
+merged_stagm_stag<-merged_stagm_stag[! merged_stagm_stag$Installation %in% drp,]
 
-#concate Inst,Plot,STP,Tree
+#Removes tree records where year measurement=year growth
+merged_stagm_stag<-merged_stagm_stag[!merged_stagm_stag$Year_Measurement==merged_stagm_stag$Year_Growth,]
+
+#Removes all non-PIPO tree records
+merged_stagm_stag<-merged_stagm_stag[merged_stagm_stag$Species=="PIPO",]
+
+#Concatenates Inst,Plot,STP,Tree for unique individual tree identification
 merged_stagm_stag$conc<-paste(merged_stagm_stag$Installation,merged_stagm_stag$Plot,merged_stagm_stag$STP,
                                 merged_stagm_stag$Tree,sep=",")
 
-#(height-prev.years height)/interval=annualized height growth
-
-merged_stagm_stag<-merged_stagm_stag[!merged_stagm_stag$Year_Measurement==merged_stagm_stag$Year_Growth,]
-merged_stagm_stag<-merged_stagm_stag[! merged_stagm_stag$Installation %in% drp,]
-
-#Select only PIPO trees
-merged_stagm_stag<-merged_stagm_stag[merged_stagm_stag$Species=="PIPO",]
 
 #This function checks the year of measurement in the tree record against
-#the measurement timeline, finds the next measurement year's height,
-#then subtracts the height of the first year of measurement to provide a height increment
+#the measurement timeline, finds the previous measurement year's height,
+#then subtracts the height in the first year of measurement to provide a 
+#height increment that is assigned to the 
 
 annual.ht<-function(conca,year){
-  
+  #creates dataframe of an individual small tree in all meas years
   treeinfo<-merged_stagm_stag[merged_stagm_stag$conc==conca,]
+  #selects all treeinfo records in years that are not found in timeline
   next.year <- timeline[timeline$Installation==treeinfo$Installation[1],"Year_Measurement"]
+  #removes NA from treerecords
   next.year <- next.year[!is.na(next.year)]
+  #selects tree records that are greater than specified year
   next.year <- next.year[next.year>year]
+  #selects the minimum tree record from those remaining
   next.year <- ifelse(length(next.year)==0,0,min(next.year))
+  #selects treeinfo records that correspond to specified year and "next year"
+  #produces error of -999 if nrow of treeinfo =1
+  #otherwise, subtracts spec year height from next year height and 
+  #divides by the range in the measurement years
+  #produces output "htinc" which is in units ft/year
   treeinfo <- treeinfo[treeinfo$Year_Measurement %in% c(year,next.year),]
     if (nrow(treeinfo)==1){
     htinc <- -999
@@ -38,17 +51,8 @@ annual.ht<-function(conca,year){
   htinc
 }
   
-
 ###Example on a single tree record
-annual.ht("LR,2,1,123",2010)
-
-#remove DF plot 4 stp1 2001 measurements
-#merged_stagm_stag<-merged_stagm_stag[!(merged_stagm_stag=='DF'&merged_stagm_stag$Plot==4&
-  #                    merged_stagm_stag$STP==1&merged_stagm_stag$Year_Measurement==2001),]
-
-#change year measurement of DF plot 4 stp1 from 2002 to 2001, was measured later due to bees nest
-#merged_stagm_stag$Year_Measurement[which(merged_stagm_stag=='DF'&merged_stagm_stag$Plot==4&
- #   merged_stagm_stag$STP==1&merged_stagm_stag$Year_Measurement==2002)]<-2001
+annual.ht("LR,2,5,138",2002)
 
 #Assign column for height annualized height growth inc
 merged_stagm_stag$ht_annual<-0
@@ -58,18 +62,11 @@ for(i in 1:nrow(merged_stagm_stag)){
     merged_stagm_stag$ht_annual[i]<-annual.ht(merged_stagm_stag$conc[i], merged_stagm_stag$Year_Measurement[i])
 }
 
-#Remove all records with ht_annual=NA,-999
+#Remove all records with ht_annual=NA,-999, 
+#these represent end-of-timeline-interval tree records
 merged_stagm_stag$ht_annual[is.na(merged_stagm_stag$ht_annual)]<--999     
 merged_stagm_stag<-merged_stagm_stag[!(merged_stagm_stag$ht_annual==-999),]
 
 #Rename dataframe something reasonable
 annual.gr<-merged_stagm_stag
               
-                                     
-
-
-
-
-
-
-

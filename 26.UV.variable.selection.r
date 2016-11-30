89d8ccbab06c846cecf7f53a9f329d034097b5de
kirkmire <kirkmirecp@gmail.com>
2016-11-30 11:44:03 -0700

    11/30

--- 26.UV.variable.selection.r
+++ 26.UV.variable.selection.r
@@ -1,31 +1,50 @@
-
 ######Understory Non-Tree Variables######
 #Percentage cover 1m, upper and lower extent of height
 
 
 
-
-#Selecting installations 
+#Selects all installations 
 veg<-c("EM","BC","TJ","RM","CM","TC")
 
 annual.gr3<-annual.gr[annual.gr$Installation %in% sim, ]
 
+#Abbreviates row headings in veg plot measurements
+names(sstp1)[7:8]<-c("Cov","Bas")
 
-#Reshape 1 meter small tree plot veg measurements so that each stp record year has its
+#Reshaped 1 meter small tree plot veg measurements so that each stp record year has its
 #own associated Forb, Grass, Low Shrub, High Shrub and Polyveg measurements
 
 stp<-reshape(sstp1, direction="wide",idvar=
                      c("Installation","Plot","STP","Year_Measurement"),
-                   timevar="Lifeform",v.names=c("Coverage","Base","Top"))
+                   timevar="Lifeform",v.names=c("Cov","Bas","Top"))
 
-#Merge plot history and stp 1m veg
+#Makes NAs within coverage, base, and top veg meas columns = 0
+veg.names<-names(stp[,substring(names(stp),4,4)=="."])
 
+for(i in veg.names) {
+  stp[i][is.na(stp[i])] <- 0
+}
+
+#Merges plot history (trt etc) and stp 1m veg meas
 veg_record<- merge(splot, stp,by=c("Installation","Plot"))
 
 
-#Merge annual small tree growth with veg_record for each year
-
+#Merges annual small tree growth records with veg records for each year
 veg_record1<- merge(annual.gr3, veg_record,by=c("Installation","Plot","STP","Year_Measurement"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
